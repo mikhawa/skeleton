a558afd324007427f3af278524274b992f53c067
Pitz Michaël <michaeljpitz@gmail.com>
2018-10-03 16:38:56 +0200

    php bin/console make:controller ArticlestController

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class ArticlesController extends AbstractController
+{
+    /**
+     * @Route("/articles", name="articles")
+     */
+    public function index()
+    {
+        return $this->render('articles/index.html.twig', [
+            'controller_name' => 'ArticlesController',
+        ]);
+    }
+}
