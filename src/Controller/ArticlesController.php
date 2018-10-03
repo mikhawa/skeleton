514beca3fc425c552f519b8e34cce7a6d134418f
Pitz Michaël <michaeljpitz@gmail.com>
2018-10-03 16:52:12 +0200

    to modify controller test

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -4,17 +4,38 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Doctrine\ORM\Mapping as ORM;
+use App\Entity\Articles;
+use App\Entity\Users;
 
 class ArticlesController extends AbstractController
 {
     /**
-     * @Route("/articles", name="articles")
+     * @Route("/", name="articles")
      */
     public function index()
     {
-        return $this->render('articles/index.html.twig', [
-            'controller_name' => 'ArticlesController',
-        ]);
+        // you can fetch the EntityManager via $this->getDoctrine()
+        // or you can add an argument to your action: index(EntityManagerInterface $entityManager)
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $product = new Articles();
+        $util= new Users();
+        $one = $this->getDoctrine()
+            ->getRepository(Users::class)
+            ->find(1);
+
+        $product->setTitle('Keyboard');
+        $product->setContent('Keyboard for exemple');
+        $product->setUsersusers($one->getIdusers());
+        $product->setTemps(new \DateTime());
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($product);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new product with id '.$product->getIdarticles());
     }
 }
