d713c4cc061e6a02926f42716897480d273f34b7
Pitz Michaël <michaeljpitz@gmail.com>
2018-10-08 09:57:50 +0200

    Create table Administrateur with php bin/console make:user

--- src/Migrations/Version20181008075403.php
+++ src/Migrations/Version20181008075403.php
@@ -8,16 +8,14 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20181007184813 extends AbstractMigration
+final class Version20181008075403 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE articles DROP FOREIGN KEY FK_BFDD31683286BD3A');
-        $this->addSql('DROP TABLE articles');
-        $this->addSql('DROP TABLE users');
+        $this->addSql('CREATE TABLE administrateur (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_32EB52E8F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
@@ -25,5 +23,6 @@ final class Version20181007184813 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('DROP TABLE administrateur');
     }
 }
