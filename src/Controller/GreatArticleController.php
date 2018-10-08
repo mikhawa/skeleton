6708ab5acade2193da266c2605fcfa98cbd728a2
Pitz Michaël <michaeljpitz@gmail.com>
2018-10-08 10:43:38 +0200

    create security for connection

--- src/Controller/GreatArticleController.php
+++ src/Controller/GreatArticleController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 
 
@@ -41,6 +42,22 @@ class GreatArticleController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils)
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+    }
 
     /**
      * @Route("/admin", name="great_article_index", methods="GET")
